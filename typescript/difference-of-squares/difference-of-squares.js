074acde25126e984f153257086e26f3aff6a0b5e
Rafał Łukawski <rs.lukawski@gmail.com>
2021-11-05 06:42:53 +0100

    difference of squares

---
+++ typescript/difference-of-squares/difference-of-squares.js
@@ -0,0 +1,17 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+class Squares {
+    constructor(count) {
+        throw new Error('Remove this statement and implement this function');
+    }
+    get sumOfSquares() {
+        throw new Error('Remove this statement and implement this function');
+    }
+    get squareOfSum() {
+        throw new Error('Remove this statement and implement this function');
+    }
+    get difference() {
+        throw new Error('Remove this statement and implement this function');
+    }
+}
+exports.Squares = Squares;
