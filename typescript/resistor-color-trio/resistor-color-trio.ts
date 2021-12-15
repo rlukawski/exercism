691ffac7d30dd74364f0b3f102bc6d5793456927
Rafał Łukawski <rs.lukawski@gmail.com>
2021-12-15 12:23:59 +0100

    resistor color solution

--- typescript/resistor-color-trio/resistor-color-trio.ts
+++ typescript/resistor-color-trio/resistor-color-trio.ts
@@ -1,16 +1,15 @@
 
-const colors:string[] = [
-  "black","brown","red","orange","yellow","green","blue","violet","grey","white"
-];
+
 
 export function decodedResistorValue(c:string[]):string {
   const find = (c:string) => {
+    const colors:string[] = [
+      "black","brown","red","orange","yellow","green","blue","violet","grey","white"
+    ];    
     return colors.findIndex((item) => item === c);
   }
 
-  let n = find(c[0]) * 10 + find(c[1]);
-  let m = 10 ** find(c[2]);
-  n = n * m;
+  let n = (find(c[0]) * 10 + find(c[1]) )* 10 ** find(c[2]);
   let units = "ohms";
   if( n > 1000 ) {
     units = "kiloohms";
