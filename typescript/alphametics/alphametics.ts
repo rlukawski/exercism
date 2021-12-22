84f0b7efef8d8308ab44b42ef785bd69c4ac276e
Rafał Łukawski <rs.lukawski@gmail.com>
2021-12-22 12:31:06 +0100

    alphametics solution

--- typescript/alphametics/alphametics.ts
+++ typescript/alphametics/alphametics.ts
@@ -1,24 +1,15 @@
 type Letters = {[key:string]:number};
 
 function sumWords(dictionary:Letters, words: string[]): number {
-  let mywords = [...words];
-  for(let l in dictionary) {
-    for( let idx in mywords) {
-      let re = new RegExp(l,"g");
-      mywords[idx] = mywords[idx].replace(re,String(dictionary[l]));
-    }
-  }
-
   let sum = 0;
-  for( let word of mywords) {
-    sum+=Number(word);
+  for(let word of [...words]) {
+    sum  += Number(word.split('').map(item => dictionary[item]).join(''));
   }
-  
   return sum;
 }
 
 
-function checkPuzzle(dictionary:Letters, wl:string[],wr:string[]) {
+function checkPuzzle(dictionary:Letters, wl:string[],wr:string[]):boolean {
   for(let word of wl) {
     if( dictionary[word[0]]===0) {
       return false;
@@ -35,19 +26,25 @@ function checkPuzzle(dictionary:Letters, wl:string[],wr:string[]) {
   return false;
 }
 
-function generatePermutations(result:Letters[],dictionary:Letters,idx:number = 0, list:number[]=[0,1,2,3,4,5,6,7,8,9]) {
-
+function generatePermutations(wl:string[], wr:string[], dictionary:Letters,idx:number = 0, list:number[]=[0,1,2,3,4,5,6,7,8,9]):Letters[] {
+  let output:Letters[] = [];
 
   for( let np in list ) {
     let key = Object.keys(dictionary)[idx];
     dictionary[key] = list[np];
-    
     if( idx<Object.keys(dictionary).length -1 ) {
-      generatePermutations(result,dictionary,idx+1,list.filter(item => item !== list[np]));
+      output = generatePermutations(wl, wr, dictionary,idx+1,list.filter(item => item !== list[np]));
+      if(output.length) {
+        break;
+      }
     } else {
-      result.push({...dictionary});
+      if( checkPuzzle(dictionary,wl,wr)) {
+        output.push({...dictionary});
+        break;
+      }
     }
   }
+  return output;
 }
 
 export function solve(puzzle: string): Letters {
@@ -56,22 +53,11 @@ export function solve(puzzle: string): Letters {
   let sides = puzzle.split('==');
   let wordsLeft:string[] = sides[0].split('+');
   let wordsRight:string[] = sides[1].split('+');
-
   let letters = puzzle.replace(/[^A-Z]/g,'');
   letters.split('').forEach(letter => {dictionary[letter] = 0});
-  let result:Letters[] = [];
-  generatePermutations(result,dictionary);
-  // console.log(Object.keys(result).length);
+  
   let output: Letters[] = [];
-  for( let i of result) {
-    if( checkPuzzle(i,wordsLeft,wordsRight) ) {
-      output.push(i);
-    }
-  }
+  output = generatePermutations(wordsLeft, wordsRight, dictionary);
   return output[0];
 }
-
-
-// const data='AND + A + STRONG + OFFENSE + AS + A + GOOD == DEFENSE';
-// console.log(solve(data));
 
